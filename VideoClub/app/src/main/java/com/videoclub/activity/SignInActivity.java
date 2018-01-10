a931c4967a9a768e9c176c1ace3fd22c053e4c28
Bagias Nikolaos <fixergaming@gmail.com>
2018-01-10 23:48:47 +0200

    Create UI skeleton for SignIn activity.

--- VideoClub/app/src/main/java/com/videoclub/activity/SignInActivity.java
+++ VideoClub/app/src/main/java/com/videoclub/activity/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.videoclub.activity;
 
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,18 +11,27 @@ import com.videoclub.entity.User;
 
 import java.util.List;
 
-public class SplashActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-
-
+        setContentView(R.layout.activity_sign_in);
+        // First handle UI related operations.
+        setupUi();
+        // Initialize database in the background.
         new DatabaseAsync().execute();
+    }
+
+    private void setupUi() {
+        // Hide app title from the toolbar.
+        ActionBar toolbar = getSupportActionBar();
+        if (toolbar != null)
+            toolbar.setDisplayShowTitleEnabled(false);
+
+
     }
 
     class DatabaseAsync extends AsyncTask<Void, Void, Void> {
