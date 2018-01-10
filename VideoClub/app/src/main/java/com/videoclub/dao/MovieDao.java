f924276a6ca38f88defbbb9afba026f8a5a89c3c
koskarajohn <kvkarag@gmail.com>
2018-01-10 21:24:02 +0200

    dao

---
+++ VideoClub/app/src/main/java/com/videoclub/dao/MovieDao.java
@@ -0,0 +1,30 @@
+package com.videoclub.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.videoclub.entity.Movie;
+
+import java.util.List;
+
+/**
+ * Created by Kostas on 1/10/2018.
+ */
+
+@Dao
+public interface MovieDao {
+
+    @Query("SELECT * FROM user")
+    List<Movie> getAllMovies();
+
+    @Insert
+    void insertMovie(Movie movie);
+
+    @Insert
+    void insertAllMovies(Movie... movies);
+
+    @Delete
+    void deleteMovie(Movie movie);
+}
