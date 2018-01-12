e6664b417a27a9d78d3bd9d7c399f4fbc566911d
Bagias Nikolaos <fixergaming@gmail.com>
2018-01-12 18:37:27 +0200

    Add custom toolbar, show movie title at info toolbar.

--- VideoClub/app/src/main/java/com/videoclub/ui/activity/HomeActivity.java
+++ VideoClub/app/src/main/java/com/videoclub/ui/activity/HomeActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.videoclub.data.database.entity.User;
 import com.videoclub.data.helpers.Constants;
@@ -39,6 +42,24 @@ public class HomeActivity extends AppCompatActivity implements MovieAdapter.Movi
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.d(TAG, "Settings selected");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onMovieClicked(Movie selectedMovie) {
         Intent intent = new Intent(this, InfoActivity.class);
         intent.putExtra(Constants.EXTRA_MOVIE_TITLE, selectedMovie.getName());
@@ -46,6 +67,9 @@ public class HomeActivity extends AppCompatActivity implements MovieAdapter.Movi
     }
 
     private void setupUi(){
+        // Setup action bar.
+        Toolbar toolbar = findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
         // Get a reference to the recycler view.
         recyclerView = findViewById(R.id.home_movies_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
--- VideoClub/app/src/main/java/com/videoclub/ui/activity/InfoActivity.java
+++ VideoClub/app/src/main/java/com/videoclub/ui/activity/InfoActivity.java
@@ -3,6 +3,7 @@ package com.videoclub.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -81,6 +82,10 @@ public class InfoActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setupUi() {
+        // Setup action bar.
+        Toolbar toolbar = findViewById(R.id.app_toolbar);
+        toolbar.setTitle(movieTitle);
+        setSupportActionBar(toolbar);
         // Add click handler to reserve button.
         btnReserve = findViewById(R.id.info_movie_reserve_button);
         btnReserve.setOnClickListener(this);
